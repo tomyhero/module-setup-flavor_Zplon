4c1aa3e1b8fa515deda57b99e389312ca5c2c582
Tomohiro Teranishi <tomyhero@zx.local>
2011-12-09 20:54:40 +0900

    mv setup.sh location

--- Ze/Helper/Zplon.pm
+++ Ze/Helper/Zplon.pm
@@ -90,39 +90,6 @@ template: |
   auto_include;
   WriteAll;
 ---
-file: setup.sh
-template: |+
-  #!/bin/bash
-  
-  
-  APP_HOME='.'
-  MYRC=$HOME/.`basename $SHELL`rc
-  DATABASE_NAME=[% dist | lower %]_${[% dist | upper %]_ENV}
-  
-  
-  
-  
-  #* 環境変数
-  if [ ! $[% dist | upper %]_ENV ];then
-  echo "export [% dist | upper %]_ENV=local" >> $MYRC
-  source $MYRC
-  fi
-  
-  
-  
-  HAS_DB=`echo 'show databases' | mysql -u root | grep $DATABASE_NAME | wc -l`
-  
-  if [ $HAS_DB == 1 ]
-  then
-      echo 'HAS database'
-  else
-      mysqladmin -u root create $DATABASE_NAME
-      mysql -u root $DATABASE_NAME < $APP_HOME/misc/[% dist | lower %].sql
-  fi
-  
-  
-
----
 file: bin/filegenerator.pl
 template: |+
   #!/usr/bin/env perl
@@ -165,6 +132,44 @@ template: |
   
   
   cpanm --mirror ftp://ftp.kddilabs.jp/CPAN/ --installdeps .
+---
+file: bin/devel/setup.sh
+template: |+
+  #!/bin/bash
+  
+  
+  APP_HOME='.'
+  MYRC=$HOME/.`basename $SHELL`rc
+  DATABASE_NAME=[% dist | lower %]_${[% dist | upper %]_ENV}
+  
+  
+  if [ ! -f $HOME/bin/devel/setup.sh ]; then
+      echo 'you must excute this script from application home directory!! like a ./bin/devel/setup.sh'
+      exit(0);
+  fi
+  
+  
+  
+  #* 環境変数
+  if [ ! $[% dist | upper %]_ENV ];then
+  echo "export [% dist | upper %]_ENV=local" >> $MYRC
+  source $MYRC
+  fi
+  
+  
+  
+  HAS_DB=`echo 'show databases' | mysql -u root | grep $DATABASE_NAME | wc -l`
+  
+  if [ $HAS_DB == 1 ]
+  then
+      echo 'HAS database'
+  else
+      mysqladmin -u root create $DATABASE_NAME
+      mysql -u root $DATABASE_NAME < $APP_HOME/misc/[% dist | lower %].sql
+  fi
+  
+  
+
 ---
 file: bin/devel/ukigumo-client.pl
 template: |
